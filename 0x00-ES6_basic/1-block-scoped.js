c416f99eff090882380d28a4fcbdab169836d9dd
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-11-28 03:43:33 +0300

    Update 1-block-scoped.js
    - Assign the inner variables to outer scope variables

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,11 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const innerTask = false;
+    const innerTask2 = true;
+    
+    task = innerTask; // Assign the inner variables to outer scope variables
+    task2 = innerTask2; // Assign the inner variables to outer scope variables
   }
 
   return [task, task2];
