abdee39cf29d4a5a7f172e84d2a0d02e3c9baaf1
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-11-28 03:48:21 +0300

    Update 1-block-scoped.js
    - Declare task and task2 as const

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    const innerTask = false;
-    const innerTask2 = true;
-    
-    task = innerTask; // Assign the inner variables to outer scope variables
-    task2 = innerTask2; // Assign the inner variables to outer scope variables
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
