ba177d3b9602c77a38fa3aa71ea1236bbb835686
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-11-29 13:13:25 +0300

    Update 6-final-user.js
    - Convert error into string

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -9,7 +9,7 @@ async function handleProfileSignup(firstName, lastName, fileName) {
 
   return results.map((result) => ({
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
+    value: result.status === 'fulfilled' ? result.value : String(result.reason),
   }));
 }
 
