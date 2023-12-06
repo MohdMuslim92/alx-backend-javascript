6a0ffe205dacb86401e54affd68b6f2f764e11f3
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-12-06 22:42:50 +0300

    Update 8-clean_set.js
    - Add checks if startString is a string and ensures that the provided set is
      indeed an instance of Set.and then filters the values based on the condition
      and generates the final string accordingly

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
 function cleanSet(set, startString) {
-  if (!set || !(set instanceof Set) || !startString || !startString.length) {
+  if (!set || !(set instanceof Set) || !startString || typeof startString !== 'string') {
     return '';
   }
 
   const filteredValues = [...set].filter((value) => value.startsWith(startString));
-  const cleanedString = filteredValues.map((value) => value.slice(startString.length)).join('-');
+  const cleanedString = [...filteredValues].map((value) => value.slice(startString.length)).join('-');
   return cleanedString;
 }
 
