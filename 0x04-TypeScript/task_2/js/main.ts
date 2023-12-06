b4ca4be25ceecbd6e73cc6ea13edf78f23765c09
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-12-06 17:48:35 +0300

    Update task_2/js/main.ts
    - Add Subjects String Literal Type:
        type Subjects = 'Math' | 'History'; defines a type Subjects that can only
          have the values 'Math' or 'History'.
    - Add teachClass function:
        function teachClass(todayClass: Subjects): string { ... } takes an argument
          todayClass of type Subjects.
        Inside the function, it checks the value of todayClass:
            If todayClass is 'Math', it returns the string 'Teaching Math'.
            If todayClass is 'History', it returns the string 'Teaching History'.
            If todayClass is neither 'Math' nor 'History', it returns
    	'Invalid class' for handling unexpected values.

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -50,10 +50,12 @@ console.log(createEmployee(200)); // Output: Teacher
 console.log(createEmployee(1000)); // Output: Director
 console.log(createEmployee('$500')); // Output: Director
 
+// function isDirector used as a type predicate
 function isDirector(employee: Director | Teacher): employee is Director {
   return 'workDirectorTasks' in employee;
 }
 
+// function executeWork calls workDirectorTasks or workTeacherTasks
 function executeWork(employee: Director | Teacher): string {
   if (isDirector(employee)) {
     return employee.workDirectorTasks();
@@ -64,3 +66,20 @@ function executeWork(employee: Director | Teacher): string {
 
 console.log(executeWork(createEmployee(200))); // Output: Getting to work
 console.log(executeWork(createEmployee(1000))); // Output: Getting to director tasks
+
+//function teachClass return Teaching Math or Teaching History
+type Subjects = 'Math' | 'History';
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else if (todayClass === 'History') {
+    return 'Teaching History';
+  } else {
+    // Handling for unexpected values
+    return 'Invalid class';
+  }
+}
+
+console.log(teachClass('Math')); // Output: Teaching Math
+console.log(teachClass('History')); // Output: Teaching History
