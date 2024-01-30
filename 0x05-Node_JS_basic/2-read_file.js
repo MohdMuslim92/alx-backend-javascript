65e04293e7e998e8b04f48c82965efbe2ab68776
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-01-31 00:44:37 +0300

    Update 2-read_file.js, remove extra space

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -6,7 +6,7 @@
 
 const fs = require('fs');
 
-function countStudents (path) {
+function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
     const lines = data.trim().split('\n');
