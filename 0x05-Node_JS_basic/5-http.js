5f0d5063e30bb79435edcc252a5841622b988620
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-02-01 14:00:04 +0300

    Creates an HTTP server that responds with different messages
    - The responce is based on the requested URL path, including student
    - information if the path is '/students'

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,34 @@
+/** Creates an HTTP server that responds with different messages based on the
+ * requested URL path, including student information if the path is
+ * '/students'.
+ */
+const http = require('http');
+const url = require('url');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const { pathname } = parsedUrl;
+
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  if (pathname === '/') {
+    res.end('Hello Holberton School!\n');
+  } else if (pathname === '/students') {
+    const [, , database] = process.argv;
+
+    countStudents(database)
+      .then(() => {
+        res.end();
+      })
+      .catch((error) => {
+        res.end(error.message);
+      });
+  } else {
+    res.end('Not found\n');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
