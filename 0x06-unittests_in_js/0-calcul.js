ed4ddca735f8c98e4278a587d8477c5fa5d1d50f
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-02-02 01:44:42 +0300

    Implements a function calculateNumber that rounds two numbers and returns their sum

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,7 @@
+// Implements a function calculateNumber that rounds two numbers and returns their sum.
+
+function calculateNumber (a, b) {
+  return Math.round(a) + Math.round(b);
+}
+
+module.exports = calculateNumber;
