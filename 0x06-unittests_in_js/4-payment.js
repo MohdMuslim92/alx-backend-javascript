785fdf27fea64e269e019f43b465fba11b5fcf6d
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2024-02-02 03:14:17 +0300

    Module defining sendPaymentRequestToApi function for simulating payment
    requests, utilizing calculateNumber from the Utils module.

---
+++ 0x06-unittests_in_js/4-payment.js
@@ -0,0 +1,12 @@
+/** Module defining sendPaymentRequestToApi function for simulating payment
+ * requests, utilizing calculateNumber from the Utils module.
+ */
+
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi (totalAmount, totalShipping) {
+  const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${result}`);
+}
+
+module.exports = sendPaymentRequestToApi;
